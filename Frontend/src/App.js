ec2f3d3ebc7d403db93fbec0e51de203538194b9
SanchiiBuilds <sanchiarora036@gmail.com>
2026-06-24 12:15:14 +0530

    added changes for offer ride

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,6 +6,7 @@ import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import RecurringRide from "./pages/RecurringRide";
+import HopinOfferRide from "./components/HopinOfferRide";
 
 function Placeholder({ title }) {
   return (
