d750440a1a308bcaba30d8d35acd0d84f70cfd42
shreshtha-a <shreshthaanand30@gmail.com>
2026-06-27 22:03:34 +0530

    fixed the offer ride error

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -166,7 +166,10 @@ export default function Home() {
         </div>
 
         {/* OFFER RIDE */}
-        <div style={styles.actionCard} onClick={() => navigate("/create-ride")}>
+        <div
+  style={styles.actionCard}
+  onClick={() => navigate("/offer-ride")}
+>
           {/* Icon */}
           <div
             style={{
@@ -188,7 +191,7 @@ export default function Home() {
           {/* Text */}
           <div style={{ flex: 1 }}>
             <div style={{ fontSize: "15px", fontWeight: "700", color: "#0F2D52" }}>
-              Create Ride
+              Offer Ride
             </div>
             <div style={{ fontSize: "12px", color: "#64748B", marginTop: "2px" }}>
               Publish your trip &amp; earn
